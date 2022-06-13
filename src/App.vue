0f6255ad9b6d4cc6c1bd52fb19b738ca8c9a7572
张超嘉 <602173141@qq.com>
2022-06-13 08:58:12 +0800

    feat: 初始化工程

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,15 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
+* {
+  margin: 0px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,8 +22,6 @@
 }
 
 #nav {
-  padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
