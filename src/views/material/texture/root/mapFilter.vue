33eca3407f57b9a6eab91c8ae79ec1388511ca3b
张超嘉 <602173141@qq.com>
2022-06-20 14:00:25 +0800

    continue: 纹理采样方式案例demo

--- src/views/material/texture/root/mapFilter.vue
+++ src/views/material/texture/root/mapFilter.vue
@@ -30,8 +30,15 @@ export default {
       const axesHelper = new THREE.AxesHelper(5)
       scene.add(axesHelper)
       const geometry = new THREE.BoxGeometry(1, 1, 1)
+      const textureImg = require(`@/assets/mesh/minecraft.png`)
+      const texture = new THREE.TextureLoader().load(textureImg)
+      //   texture.minFilter = THREE.NearestFilter
+      // 一张小纹理贴到一个大空间（例如16X16的纹理映射到32X32的像素空间），相当于纹理拉大
+      //   参考：https://juejin.cn/post/7053621679762702343#comment
+      texture.magFilter = THREE.NearestFilter
       const material = new THREE.MeshBasicMaterial({
-        color: 0x00ff00,
+        color: "#ffff00",
+        map: texture,
       })
       const cube = new THREE.Mesh(geometry, material)
       scene.add(cube)
